334b71c6a4fe803cc590cdd6b81f0f4b9ebaa0a8
DQNEO <dqneoo@gmail.com>
2014-06-29 16:33:12 +0900

    10.1 lsコマンドを作る

---
+++ 10/ls.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+static void do_ls(char *path);
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    if (argc <= 1) {
+	fprintf(stderr, "no arguments\n");
+	return 1;
+    }
+
+
+
+    for (i = 1; i < argc  ; i++) {
+	do_ls(argv[i]);
+    }
+    return 0;
+}
+
+static void do_ls(char *path)
+{
+
+    DIR *dp;
+    struct dirent *dent;
+
+    dp = opendir(path);
+    if (dp == NULL) {
+	perror(path);
+	exit(1);
+    }
+
+    while ((dent = readdir(dp)) != NULL) {
+	printf("%s  ", dent->d_name);
+    }
+
+    printf("\n");
+}
