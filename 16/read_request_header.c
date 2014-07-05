45d2995eef388b7e34a3ed023bce0f2428d6cc36
DQNEO <dqneoo@gmail.com>
2014-07-05 15:55:25 +0900

    1行目のパーズができるようになった

---
+++ 16/read_request_header.c
@@ -0,0 +1,44 @@
+/**
+ * 標準入力からHTTP Request Headerを受け取って
+ * １行目をパーズするプログラム
+ * Usage: ./read_request_header < request_header.txt
+ */
+#include <stdio.h>
+#include <string.h>
+
+#define BUF_MAX_LEN 1024
+
+int main()
+{
+    FILE *fin;
+    char buf[BUF_MAX_LEN];
+    char *p;
+
+    char method[BUF_MAX_LEN];
+    char path[BUF_MAX_LEN];
+    char proto[BUF_MAX_LEN];
+
+    fin = stdin;
+
+    if (fgets(buf, BUF_MAX_LEN, fin) == NULL) {
+	perror("fgets");
+	return 1;
+    }
+
+    p = strchr(buf, ' ');
+    *p = '\0';
+    strcpy(method, buf);
+
+    p++;
+    p = strchr(p, ' ');
+    *p = '\0';
+    strcpy(path, buf + strlen(method) + 1);
+    p++;
+    strcpy(proto, p);
+
+    printf("=== result ===\n");
+    printf("method:%s\n", method);
+    printf("path:%s\n", path);
+    printf("proto:%s\n", proto);
+    return 0;
+}
